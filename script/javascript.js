01e9d17cbec6b8e0acc73ebd966631c1034ebf98
Jay Morgan <jaymorgan@jays-mbp-2.home>
2014-03-14 23:35:34 +0000

    Update to parallax effect, inclusion of styling

--- script/javascript.js
+++ script/javascript.js
@@ -1,19 +1,28 @@
 $(document).ready(function(){
     var defaultX = $('#wrapper').css('left');
     var defaultY = $('#wrapper').css('top');
-    var defaultXNode = $('#node').css('left');
-    var defaultYNode = $('#node').css('top');
-
     var moveAmountX = $('#wrapper').css('left');
     var moveAmountY = $('#wrapper').css('top');
-    var moveAmountXNode = $('#node').css('left');
-    var moveAmountYNode = $('#node').css('top');
+
+    var defaultYNode = $('.node').css('top');
+    var defaultXNode = $('.node').css('left');
+    var moveAmountXNode = $('.node').css('left');
+    var moveAmountYNode = $('.node').css('top');
+
+    var defaultYSelected = $('.selected').css('top');
+    var defaultXSelected = $('.selected').css('left');
+    var moveAmountXSelected = $('.selected').css('left');
+    var moveAmountYSelected = $('.selected').css('top');
 
     var defaultXBody = $('body').css('left');
     var defaultYBody = $('body').css('top');
-
     var moveAmountXBody = $('body').css('left');
     var moveAmountYBody = $('body').css('top');
+
+    var defaultXFooter = $('#footer').css('left');
+    var defaultYFooter = $('#footer').css('top');
+    var moveAmountXFooter = $('#footer').css('left');
+    var moveAmountYFooter = $('#footer').css('top');
 
     var currentMousePos = {x: -1, y: -1};
 
@@ -21,7 +30,6 @@ $(document).ready(function(){
         $(document).mousemove(function (event) { 
             currentMousePos.x = event.pageX;
             currentMousePos.y = event.pageY;
-
 
             /* Wrapper*/
             var moveX = (parseInt(moveAmountX) - parseInt(currentMousePos.x)) * 0.05;
@@ -33,21 +41,46 @@ $(document).ready(function(){
             $('#wrapper').css("top", movingY);
 
             /* Nodes */
-            var moveXNode = (parseInt(moveAmountXNode) - parseInt(currentMousePos.x)) * 0.06;
-            var moveYNode = (parseInt(moveAmountYNode) - parseInt(currentMousePos.y)) * 0.06;
+            var moveXNode = (parseInt(moveAmountXNode) - parseInt(currentMousePos.x)) * 0.01;
+            var moveYNode = (parseInt(moveAmountYNode) - parseInt(currentMousePos.y)) * 0.01;
             var movingXNode = parseInt(defaultXNode) + parseInt(moveXNode);
             var movingYNode = parseInt(defaultYNode) + parseInt(moveYNode);
 
-            $('#node').css("left", movingXNode);
-            $('#node').css("top", movingYNode);
+            $('.node').css("left", movingXNode);
+            $('.node').css("top", movingYNode);
+
+            /* Nodes Selected */
+            var moveAmountXSelected = (parseInt(moveAmountXSelected) - parseInt(currentMousePos.x)) * 0.01;
+            var moveAmountYSelected = (parseInt(moveAmountYSelected) - parseInt(currentMousePos.y)) * 0.01;
+            var movingXSelected = parseInt(defaultXSelected) + parseInt(moveXNode);
+            var movingYSelected = parseInt(defaultYSelected) + parseInt(moveYNode);
+
+            $('.selected').css("left", movingXNode);
+            $('.selected').css("top", movingYNode);
+
+            /* Footer */ 
+            var moveXFooter = (parseInt(moveAmountXFooter) - parseInt(currentMousePos.x)) * 0.1;
+            var moveYFooter = (parseInt(moveAmountYFooter) - parseInt(currentMousePos.y)) * 0.1;
+            var movingXFooter = parseInt(defaultXFooter) + parseInt(moveXFooter);
+            var movingYFooter = parseInt(defaultYFooter) + parseInt(moveYFooter);
+
+            $('#footer').css("left", movingXFooter);
+            $('#footer').css("top", movingYFooter);
 
             /* Body */
-            var moveXBody = (parseInt(moveAmountXBody) - parseInt(currentMousePos.x)) * 0.01;
-            var moveYBody = (parseInt(moveAmountYBody) - parseInt(currentMousePos.y)) * 0.01;
+            var moveXBody = (parseInt(moveAmountXBody) - parseInt(currentMousePos.x)) * 0.007;
+            var moveYBody = (parseInt(moveAmountYBody) - parseInt(currentMousePos.y)) * 0.007;
             var movingXBody = parseInt(defaultXBody) + parseInt(moveXBody);
             var movingYBody = parseInt(defaultYBody) + parseInt(moveYBody);
 
-            $('body').css('background-position', movingXBody + 'px ' + movingYBody + 'px ');
+            if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) { 
+                $('body').css('background-position-x', movingXBody + 'px ');
+                $('body').css('background-position-y', movingYBody + 'px ');
+            } else { 
+                $('body').css('background-position', movingXBody + 'px ' + movingYBody + 'px ');
+            }
+
+
 
             // alpha testing purposes
             $("span:first").text("(e.pageX)=  " + currentMousePos.x);
