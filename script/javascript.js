c7a58fe014269366c48671bb16d8899632a8437f
jaypmorgan <jaymiles17@gmail.com>
2014-03-17 02:30:09 +0000

    Adding support for accelerometer enabled device such as iphone. This will be used to control the parallax

--- script/javascript.js
+++ script/javascript.js
@@ -26,68 +26,127 @@ $(document).ready(function(){
 
     var currentMousePos = {x: -1, y: -1};
 
-
+      if(window.DeviceMotionEven==undefined) { 
         $(document).mousemove(function (event) { 
-            currentMousePos.x = event.pageX;
-            currentMousePos.y = event.pageY;
+             currentMousePos.x = event.pageX;
+             currentMousePos.y = event.pageY;
+
+             /* Wrapper*/
+             var moveX = (parseInt(moveAmountX) - parseInt(currentMousePos.x)) * 0.05;
+             var moveY = (parseInt(moveAmountY) - parseInt(currentMousePos.y)) * 0.05;
+             var movingX = parseInt(defaultX) + parseInt(moveX);
+             var movingY = parseInt(defaultY) + parseInt(moveY);
+
+             $('#wrapper').css("left", movingX);
+             $('#wrapper').css("top", movingY);
+
+             /* Nodes */
+             var moveXNode = (parseInt(moveAmountXNode) - parseInt(currentMousePos.x)) * 0.01;
+             var moveYNode = (parseInt(moveAmountYNode) - parseInt(currentMousePos.y)) * 0.01;
+             var movingXNode = parseInt(defaultXNode) + parseInt(moveXNode);
+             var movingYNode = parseInt(defaultYNode) + parseInt(moveYNode);
+
+             $('.node').css("left", movingXNode);
+             $('.node').css("top", movingYNode);
+
+             /* Nodes Selected */
+             var moveAmountXSelected = (parseInt(moveAmountXSelected) - parseInt(currentMousePos.x)) * 0.01;
+             var moveAmountYSelected = (parseInt(moveAmountYSelected) - parseInt(currentMousePos.y)) * 0.01;
+             var movingXSelected = parseInt(defaultXSelected) + parseInt(moveXNode);
+             var movingYSelected = parseInt(defaultYSelected) + parseInt(moveYNode);
+
+             $('.selected').css("left", movingXNode);
+             $('.selected').css("top", movingYNode);
+
+             /* Footer */ 
+             var moveXFooter = (parseInt(moveAmountXFooter) - parseInt(currentMousePos.x)) * 0.1;
+             var moveYFooter = (parseInt(moveAmountYFooter) - parseInt(currentMousePos.y)) * 0.1;
+             var movingXFooter = parseInt(defaultXFooter) + parseInt(moveXFooter);
+             var movingYFooter = parseInt(defaultYFooter) + parseInt(moveYFooter);
+
+             $('#footer').css("left", movingXFooter);
+             $('#footer').css("top", movingYFooter);
+
+             /* Body */
+             var moveXBody = (parseInt(moveAmountXBody) - parseInt(currentMousePos.x)) * 0.007;
+             var moveYBody = (parseInt(moveAmountYBody) - parseInt(currentMousePos.y)) * 0.007;
+             var movingXBody = parseInt(defaultXBody) + parseInt(moveXBody);
+             var movingYBody = parseInt(defaultYBody) + parseInt(moveYBody);
+
+             if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) { 
+                 $('body').css('background-position-x', movingXBody + 'px ');
+                 $('body').css('background-position-y', movingYBody + 'px ');
+             } else { 
+                 $('body').css('background-position', movingXBody + 'px ' + movingYBody + 'px ');
+             }
+
+             // alpha testing purposes
+             $("span:first").text("(e.pageX)=  " + currentMousePos.x);
+             $("span:last").text("(e.pageY)= " + currentMousePos.y);
+             $("p:first").text("(move amount x)=  " + movingXNode);
+             $("p:last").text("(move amount y)= " + movingYNode);
+
+             currentMousePos = {x: -1, y: -1}; 
+        });
+      } else { 
+         window.ondevicemotion = function (event) { 
+            x = event.acceleration.x;
+            y = event.acceleration.y;
 
             /* Wrapper*/
-            var moveX = (parseInt(moveAmountX) - parseInt(currentMousePos.x)) * 0.05;
-            var moveY = (parseInt(moveAmountY) - parseInt(currentMousePos.y)) * 0.05;
-            var movingX = parseInt(defaultX) + parseInt(moveX);
-            var movingY = parseInt(defaultY) + parseInt(moveY);
+             var moveX = (parseInt(moveAmountX) - parseInt(x)) * 0.05;
+             var moveY = (parseInt(moveAmountY) - parseInt(y)) * 0.05;
+             var movingX = parseInt(defaultX) + parseInt(moveX);
+             var movingY = parseInt(defaultY) + parseInt(moveY);
 
-            $('#wrapper').css("left", movingX);
-            $('#wrapper').css("top", movingY);
+             $('#wrapper').css("left", movingX);
+             $('#wrapper').css("top", movingY);
 
-            /* Nodes */
-            var moveXNode = (parseInt(moveAmountXNode) - parseInt(currentMousePos.x)) * 0.01;
-            var moveYNode = (parseInt(moveAmountYNode) - parseInt(currentMousePos.y)) * 0.01;
-            var movingXNode = parseInt(defaultXNode) + parseInt(moveXNode);
-            var movingYNode = parseInt(defaultYNode) + parseInt(moveYNode);
+             /* Nodes */
+             var moveXNode = (parseInt(moveAmountXNode) - parseInt(x)) * 0.01;
+             var moveYNode = (parseInt(moveAmountYNode) - parseInt(y)) * 0.01;
+             var movingXNode = parseInt(defaultXNode) + parseInt(moveXNode);
+             var movingYNode = parseInt(defaultYNode) + parseInt(moveYNode);
 
-            $('.node').css("left", movingXNode);
-            $('.node').css("top", movingYNode);
+             $('.node').css("left", movingXNode);
+             $('.node').css("top", movingYNode);
 
-            /* Nodes Selected */
-            var moveAmountXSelected = (parseInt(moveAmountXSelected) - parseInt(currentMousePos.x)) * 0.01;
-            var moveAmountYSelected = (parseInt(moveAmountYSelected) - parseInt(currentMousePos.y)) * 0.01;
-            var movingXSelected = parseInt(defaultXSelected) + parseInt(moveXNode);
-            var movingYSelected = parseInt(defaultYSelected) + parseInt(moveYNode);
+             /* Nodes Selected */
+             var moveAmountXSelected = (parseInt(moveAmountXSelected) - parseInt(x)) * 0.01;
+             var moveAmountYSelected = (parseInt(moveAmountYSelected) - parseInt(y)) * 0.01;
+             var movingXSelected = parseInt(defaultXSelected) + parseInt(moveXNode);
+             var movingYSelected = parseInt(defaultYSelected) + parseInt(moveYNode);
 
-            $('.selected').css("left", movingXNode);
-            $('.selected').css("top", movingYNode);
+             $('.selected').css("left", movingXNode);
+             $('.selected').css("top", movingYNode);
 
-            /* Footer */ 
-            var moveXFooter = (parseInt(moveAmountXFooter) - parseInt(currentMousePos.x)) * 0.1;
-            var moveYFooter = (parseInt(moveAmountYFooter) - parseInt(currentMousePos.y)) * 0.1;
-            var movingXFooter = parseInt(defaultXFooter) + parseInt(moveXFooter);
-            var movingYFooter = parseInt(defaultYFooter) + parseInt(moveYFooter);
+             /* Footer */ 
+             var moveXFooter = (parseInt(moveAmountXFooter) - parseInt(x)) * 0.1;
+             var moveYFooter = (parseInt(moveAmountYFooter) - parseInt(y)) * 0.1;
+             var movingXFooter = parseInt(defaultXFooter) + parseInt(moveXFooter);
+             var movingYFooter = parseInt(defaultYFooter) + parseInt(moveYFooter);
 
-            $('#footer').css("left", movingXFooter);
-            $('#footer').css("top", movingYFooter);
+             $('#footer').css("left", movingXFooter);
+             $('#footer').css("top", movingYFooter);
 
-            /* Body */
-            var moveXBody = (parseInt(moveAmountXBody) - parseInt(currentMousePos.x)) * 0.007;
-            var moveYBody = (parseInt(moveAmountYBody) - parseInt(currentMousePos.y)) * 0.007;
-            var movingXBody = parseInt(defaultXBody) + parseInt(moveXBody);
-            var movingYBody = parseInt(defaultYBody) + parseInt(moveYBody);
+             /* Body */
+             var moveXBody = (parseInt(moveAmountXBody) - parseInt(x)) * 0.007;
+             var moveYBody = (parseInt(moveAmountYBody) - parseInt(y)) * 0.007;
+             var movingXBody = parseInt(defaultXBody) + parseInt(moveXBody);
+             var movingYBody = parseInt(defaultYBody) + parseInt(moveYBody);
 
-            if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) { 
-                $('body').css('background-position-x', movingXBody + 'px ');
-                $('body').css('background-position-y', movingYBody + 'px ');
-            } else { 
-                $('body').css('background-position', movingXBody + 'px ' + movingYBody + 'px ');
-            }
+             if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) { 
+                 $('body').css('background-position-x', movingXBody + 'px ');
+                 $('body').css('background-position-y', movingYBody + 'px ');
+             } else { 
+                 $('body').css('background-position', movingXBody + 'px ' + movingYBody + 'px ');
+             }
 
-
-
-            // alpha testing purposes
-            $("span:first").text("(e.pageX)=  " + currentMousePos.x);
-            $("span:last").text("(e.pageY)= " + currentMousePos.y);
-            $("p:first").text("(move amount x)=  " + movingXNode);
-            $("p:last").text("(move amount y)= " + movingYNode);
-
-            currentMousePos = {x: -1, y: -1};
-        });
+             // alpha testing purposes
+             $("span:first").text("Device on Iphone=  " + x);
+             $("span:last").text("(e.pageY)= " + y);
+             $("p:first").text("(move amount x)=  " + movingXNode);
+             $("p:last").text("(move amount y)= " + movingYNode);
+         }
+      }
 });
